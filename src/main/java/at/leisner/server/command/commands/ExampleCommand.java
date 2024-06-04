ce176113edc53de025366da2468e2b5cce586832
BuildTools <unconfigured@null.spigotmc.org>
2024-06-04 22:22:58 +0200

    Update methode parameter and bodys

--- src/main/java/at/leisner/server/command/commands/ExampleCommand.java
+++ src/main/java/at/leisner/server/command/commands/ExampleCommand.java
@@ -1,8 +1,10 @@
 package at.leisner.server.command.commands;
 
+import at.leisner.server.FantaServer;
 import at.leisner.server.command.Command;
 
 import java.util.List;
+import java.util.logging.Level;
 
 public class ExampleCommand extends Command {
 
@@ -11,12 +13,12 @@ public class ExampleCommand extends Command {
     }
 
     @Override
-    public void execute(String[] args) {
-        System.out.println("Executing example command");
+    public void execute(String s, String[] strings) {
+        FantaServer.getInstance().getLogger().lang("Executing example command", Level.SEVERE);
     }
 
     @Override
-    public List<String> tabComplete(String[] args) {
+    public List<String> tabComplete(String label, String[] args) {
         // Return a list of tab completions
         return List.of("arg1", "arg2", "arg3");
     }
