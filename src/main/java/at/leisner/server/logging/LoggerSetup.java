bf792e6bb2a9ee292f09b16ba63ef1f3a61d2666
BuildTools <unconfigured@null.spigotmc.org>
2024-06-04 22:27:11 +0200

    Add GuiFormatter Return a custom Logger for Language support

--- src/main/java/at/leisner/server/logging/LoggerSetup.java
+++ src/main/java/at/leisner/server/logging/LoggerSetup.java
@@ -1,7 +1,11 @@
 package at.leisner.server.logging;
 
 
+import at.leisner.server.FantaServer;
+import at.leisner.server.gui.ServerManagerGUI;
+import at.leisner.server.lang.Language;
 import at.leisner.server.util.ColorCodes;
+import at.leisner.server.logging.Logger;
 
 import java.io.IOException;
 import java.nio.file.*;
@@ -13,7 +17,9 @@ public class LoggerSetup {
     private static final String LOG_FOLDER = "./logs";
     private static final String LOG_FILE = LOG_FOLDER + "/server.log";
     private static FileHandler fileHandler;
+    private static GUIHandler guiHandler;
     private static Logger mainLogger;
+    private static java.util.logging.Logger rootLogger;
 
     static {
         try {
@@ -24,8 +30,14 @@ public class LoggerSetup {
             fileHandler = new FileHandler(LOG_FILE, true);
             fileHandler.setFormatter(new FileFormatter());
 
+            guiHandler = new GUIHandler();
+            guiHandler.setFormatter(new GuiFormatter());
+            // Also add a console handler with color support
+            ConsoleHandler consoleHandler = new ConsoleHandler();
+            consoleHandler.setFormatter(new ConsoleFormatter());
             // Add the file handler to the root logger
-            Logger rootLogger = Logger.getLogger("");
+            rootLogger = Logger.getLogger("");
+            rootLogger.setLevel(Level.CONFIG);
             Handler[] handlers = rootLogger.getHandlers();
 
             // Remove all existing handlers to prevent duplicate logging
@@ -35,10 +47,7 @@ public class LoggerSetup {
 
             rootLogger.setUseParentHandlers(false);
             rootLogger.addHandler(fileHandler);
-
-            // Also add a console handler with color support
-            ConsoleHandler consoleHandler = new ConsoleHandler();
-            consoleHandler.setFormatter(new ConsoleFormatter());
+            rootLogger.addHandler(guiHandler);
             rootLogger.addHandler(consoleHandler);
 
         } catch (IOException e) {
@@ -46,8 +55,16 @@ public class LoggerSetup {
         }
     }
 
+    public static Logger createLogger(String name, Language language) {
+        Logger logger = new at.leisner.server.logging.Logger(name,null, language);
+        logger.setParent(rootLogger);
+        if (mainLogger == null) {
+            mainLogger = logger;
+        }
+        return logger;
+    }
     public static Logger createLogger(String name) {
-        Logger logger = Logger.getLogger(name);
+        Logger logger = new at.leisner.server.logging.Logger(name,null);
         if (mainLogger == null) {
             mainLogger = logger;
         }
@@ -61,24 +78,77 @@ public class LoggerSetup {
         public String format(LogRecord record) {
             String timestamp = dateFormat.format(new Date(record.getMillis()));
             String threadName = Thread.currentThread().getName();
+            String loggerName = record.getLoggerName();
             String level = record.getLevel().getName();
             String message = formatMessage(record);
             String color = getColor(record.getLevel());
-            return String.format("%s[%s] [%s] [%s]: %s%s%n", color, timestamp, threadName, level, message, ColorCodes.RESET);
+            return String.format("%s[%s] [%s] [%s-%s]: %s%s%n", color, timestamp, threadName, loggerName,level, message, ColorCodes.RESET);
         }
 
         private String getColor(Level level) {
             if (level == Level.SEVERE) {
-                return ColorCodes.RED;
+                return ColorCodes.RED.toString();
             } else if (level == Level.WARNING) {
-                return ColorCodes.YELLOW;
+                return ColorCodes.YELLOW.toString();
 //            } else if (level == Level.INFO) {
 //                return ColorCodes.GREEN;
             } else if (level == Level.CONFIG) {
-                return ColorCodes.BLUE;
+                return ColorCodes.BLUE.toString();
             } else {
-                return ColorCodes.WHITE;
+                return ColorCodes.WHITE.toString();
             }
+        }
+    }
+    private static class GuiFormatter extends Formatter {
+        private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        @Override
+        public String format(LogRecord record) {
+            String timestamp = dateFormat.format(new Date(record.getMillis()));
+            String threadName = Thread.currentThread().getName();
+            String loggerName = record.getLoggerName();
+            String level = record.getLevel().getName();
+            String message = formatMessage(record);
+            String color = getColor(record.getLevel());
+            return String.format("%s[%s] [%s] [%s-%s]: %s%s%n", color, timestamp, threadName, loggerName,level, message, ColorCodes.RESET);
+        }
+
+        private String getColor(Level level) {
+            if (level == Level.SEVERE) {
+                return ColorCodes.RED.toString();
+            } else if (level == Level.WARNING) {
+                return ColorCodes.ORANGE.toString();
+//            } else if (level == Level.INFO) {
+//                return ColorCodes.GREEN;
+            } else if (level == Level.CONFIG) {
+                return ColorCodes.BLUE.toString();
+            } else {
+                return ColorCodes.BLACK.toString();
+            }
+        }
+    }
+    private static class GUIHandler extends Handler {
+        private static ServerManagerGUI serverManagerGUI;
+
+
+        @Override
+        public void publish(LogRecord record) {
+            if (serverManagerGUI == null) serverManagerGUI = FantaServer.getInstance().getServerManagerGUi();
+            if (serverManagerGUI == null) return;
+            if (isLoggable(record)) {
+                String message = getFormatter().format(record);
+                serverManagerGUI.appendOutput(message);
+            }
+        }
+
+        @Override
+        public void flush() {
+
+        }
+
+        @Override
+        public void close() throws SecurityException {
+
         }
     }
 
@@ -90,8 +160,9 @@ public class LoggerSetup {
             String timestamp = dateFormat.format(new Date(record.getMillis()));
             String threadName = Thread.currentThread().getName();
             String level = record.getLevel().getName();
+            String loggerName = record.getLoggerName();
             String message = formatMessage(record);
-            return String.format("[%s] [%s] [%s]: %s%n", timestamp, threadName, level, message);
+            return String.format("[%s] [%s] [%s-%s]: %s%n", timestamp, threadName, loggerName, level, message);
         }
     }
     public static Logger getMainLogger() {
