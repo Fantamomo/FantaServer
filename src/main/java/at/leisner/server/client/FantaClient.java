043d3d6bec8e8c06d5f7c23e1222c8bbf2c3635b
BuildTools <unconfigured@null.spigotmc.org>
2024-06-04 22:23:34 +0200

    Add methode getType and getClientMetaData

--- src/main/java/at/leisner/server/client/FantaClient.java
+++ src/main/java/at/leisner/server/client/FantaClient.java
@@ -8,24 +8,37 @@ import java.net.Socket;
 public class FantaClient implements Client {
     private final Socket socket;
     private final ObjectOutputStream output;
+    private final String clientType;
+    private final FantaClientMetaData clientMetaData = new FantaClientMetaData();
 
-    public FantaClient(Socket socket) throws IOException {
+    public FantaClient(Socket socket, String clientType) throws IOException {
         this.socket = socket;
         this.output = new ObjectOutputStream(socket.getOutputStream());
+        this.clientType = clientType;
     }
-    public FantaClient(Socket socket, ObjectOutputStream output) {
+    public FantaClient(Socket socket, ObjectOutputStream output, String clientType) {
         this.socket = socket;
         this.output = output;
+        this.clientType = clientType;
     }
 
     @Override
     public void sendPacket(Packet packet) throws IOException {
         output.writeObject(packet);
-        output.flush();
     }
 
     @Override
     public void disconnect() throws IOException {
         socket.close();
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public FantaClientMetaData getClientMetaData() {
+        return clientMetaData;
     }
 }
